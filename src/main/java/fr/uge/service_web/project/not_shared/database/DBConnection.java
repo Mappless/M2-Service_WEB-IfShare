c2aa5b4dc4d6fd237679d3e91f8ee77a157b6a08
Nicolas DA COSTA <nicolas13091999@gmail.com>
2021-12-04 18:22:22 +0100

    Add DBConnection singleton

--- src/main/java/fr/uge/service_web/project/not_shared/database/DBConnection.java
+++ src/main/java/fr/uge/service_web/project/not_shared/database/DBConnection.java
@@ -1,18 +1,13 @@
 package fr.uge.service_web.project.not_shared.database;
 
-import fr.uge.service_web.project.not_shared.Config;
-import fr.uge.service_web.project.not_shared.database.exception.FailedToConnectToDatabaseException;
-
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("main-persistence-unit");
 
-    public static EntityManagerFactory getEntityManagerFactory() {
-        return emf;
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
 }
