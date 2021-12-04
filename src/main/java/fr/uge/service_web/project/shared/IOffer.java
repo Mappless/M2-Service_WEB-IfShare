5e616cc8436a068bd5bd1032000584eb4203e024
Nicolas DA COSTA <nicolas13091999@gmail.com>
2021-12-04 14:25:26 +0100

    Remove IOffer::getPurchases

--- src/main/java/fr/uge/service_web/project/shared/IOffer.java
+++ src/main/java/fr/uge/service_web/project/shared/IOffer.java
@@ -2,7 +2,6 @@ package fr.uge.service_web.project.shared;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Set;
 
 public interface IOffer extends Remote {
     int getId() throws RemoteException;
@@ -20,6 +19,4 @@ public interface IOffer extends Remote {
     int getStock() throws RemoteException;
 
     void refill(int quantity) throws RemoteException;
-
-    Set<? extends IPurchase> getPurchases() throws RemoteException;
 }
